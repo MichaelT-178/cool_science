933ab34148474b77c1df3a9f183aec70a048a56d
Michael Totaro <mtotaro@ncsu.edu>
2024-01-26 17:40:39 -0500

    Initial Commit

---
+++ js/VideoCard.js
@@ -0,0 +1,21 @@
+class VideoCard {
+    constructor(image, title, description, link) {
+        this.image = `../pics/${image}`
+        this.title = title;
+        this.description = description;
+        this.link = link
+    }
+
+    render(container) {
+        const template = document.querySelector("#video-card-template").content.cloneNode(true);
+
+        const videoLink = template.querySelector("#video-link")
+        videoLink.href = this.link;
+
+        template.querySelector('#pic').src = this.image;
+        template.querySelector('#title').innerText = this.title;
+        template.querySelector('#description').innerText = this.description;
+
+        container.appendChild(template);
+    }
+}
